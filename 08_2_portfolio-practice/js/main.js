e83390de8a6727d0f4c3d93b5f3408940a54a0a1
kimhyewon92 <sing92.naver.com>
2025-04-23 13:54:10 +0900

    임시저장

--- 08_2_portfolio-practice/js/main.js
+++ 08_2_portfolio-practice/js/main.js
@@ -16,12 +16,12 @@ spyEls.forEach(function (spyEl) {
 // Swiper 사용
 const swiper = new Swiper('.project .swiper', {
   // 슬라이드 옵션 지정
-  direction: 'vertical',  // 수직 슬라이드
-  direction: 'horizontal',  // 수평 슬라이드(기본값)
-  loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 다시 1
-  autoplay: { // 자동 재생 여부
-    delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
-  },
+    direction: 'vertical',  // 수직 슬라이드
+    direction: 'horizontal',  // 수평 슬라이드(기본값)
+    loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 다시 1
+  //   autoplay: { // 자동 재생 여부
+  //   delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+  // },
   
   // 페이지네이션 옵션
   pagination: {
@@ -35,3 +35,38 @@ const swiper = new Swiper('.project .swiper', {
     prevEl: '.project .swiper-button-prev',
   },
 });
+
+// 모달창 띄우기
+const modalBtn = document.querySelector('.project .btn-modal')
+const modalEl = document.querySelector('#modal')
+const closeBtn = document.querySelector('#modal .btn-close')
+
+const imageModalBtnList = document.querySelectorAll('.project .btn-modal-image');
+const imageModalEl = document.querySelector('#imageModal')
+const imageCloseBtn = document.querySelector('#imageModal .btn-close')
+const imageEl = document.querySelector('#imageModal img')
+
+// Quiz: 
+// style 속성: JS로 CSS 스타일을 제어할 수 있는 속성
+// 예시: 요소.style.CSS속성="";
+modalBtn.addEventListener('click', function () {
+  modalEl.style.display = 'flex';
+});
+closeBtn.addEventListener('click', function () {
+  modalEl.style.display = 'none';
+});
+
+imageModalBtnList.forEach(function (imageModalBtn, index) {
+  imageModalBtn.addEventListener('click', function () {
+    imageEl.src = imageModalBtn.dataset.imageSrc;
+    imageModalEl.style.display = 'flex';
+  })
+});
+imageCloseBtn.addEventListener('click', function () {
+  imageModalEl.style.display = 'none';
+})
+
+// 추가로 더 해볼 만한 것!
+// 모달 바깥 영역 클릭 시 닫기
+// ESC 키로 닫기
+// fade 애니메이션 넣기
